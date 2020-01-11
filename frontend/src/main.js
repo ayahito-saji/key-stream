4eba6f2ddb4b22dedb51aa92201209d267887344
AyaneMatsubara <ayataka912nerv@gmail.com>
2020-01-11 15:50:42 +0900

    Add vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -23,5 +24,6 @@ firebase.initializeApp(firebaseConfig)
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
