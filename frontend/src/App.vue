64fa15849ddfb51e7c77ef49ea04487ca880391a
AyaneMatsubara <ayataka912nerv@gmail.com>
2020-01-13 00:52:47 +0900

    Add loading animation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,9 @@
     <router-view/>
   </div>
   <div v-else>
-    Loading...
+    <div class="loading-wrap">
+      <v-progress-circular indeterminate color="blue darken-4" :size="60" :width="8" class="loading"></v-progress-circular>
+    </div>
   </div>
 </template>
 <script>
@@ -17,8 +19,17 @@
     }
   }
 </script>
-<!--<style lang="scss">
-  $color-pack: false;
+<style lang="scss">
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-  @import '~vuetify/src/styles/main.sass';
-</style>-->
+  &-wrap {
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+  }
+}
+</style>
